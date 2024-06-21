eae3f8e23261a66e86fede1cfb371b58d9d25e19
cha5s5dh <hackerone@kakao.com>
2024-06-21 13:59:22 +0900

    323123

--- main/models.py
+++ main/models.py
@@ -1,15 +1,12 @@
-# main/models.py
-
 from django.db import models
 
 class Post(models.Model):
-    number = models.AutoField(primary_key=True)
     category = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     views = models.PositiveIntegerField(default=0)
-    content = models.TextField(default='')  # 내용 필드 추가
+    content = models.TextField(default='')
 
     def __str__(self):
         return self.title
