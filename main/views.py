0346598470e3c6fe39886fa614af5e96a3da2da0
cha5s5dh <hackerone@kakao.com>
2024-06-21 21:34:47 +0900

    123

--- main/views.py
+++ main/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, 'main/home.html')
 
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'main/home.html')
+
 def security_info(request):
-    posts = Post.objects.all()
-    return render(request, 'main/security_info.html', {'posts': posts})
+    post_list = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(post_list, 15)  # 한 페이지에 15개의 게시글
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'is_paginated': page_obj.has_other_pages()
+    }
+    return render(request, 'main/security_info.html', context)
 
 def post_create(request):
     if request.method == 'POST':
--- secuportal_project/settings.py
+++ secuportal_project/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'secuportal_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'main', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +68,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'secuportal_project.wsgi.application'
 
 
@@ -78,7 +77,7 @@ WSGI_APPLICATION = 'secuportal_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
